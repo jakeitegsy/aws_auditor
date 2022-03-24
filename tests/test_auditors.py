4b75c08392ef1be04a971c91a3fd4bc00c39b9f0
jadecobra <jakeitegsy@yahoo.com>
2022-03-24 11:17:27 -0500

    add tests

---
+++ tests/test_auditors.py
@@ -0,0 +1,9 @@
+import tests.utilities
+import utilities
+
+class TestAuditors(tests.utilities.TestTemplates):
+
+    def test_auditors(self):
+        for auditor in utilities.get_auditors():
+            with self.subTest(i=auditor):
+                self.assert_templates_equal(f'audit-{auditor}')
