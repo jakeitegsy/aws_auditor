102f0d42e6e4417fe25724e508ee0a7443043517
jadecobra <jakeitegsy@yahoo.com>
2022-03-30 09:50:14 -0500

    update tests

--- architecture.py
+++ architecture.py
@@ -5,16 +5,16 @@ import diagrams.aws.integration
 import diagrams.aws.security
 
 
-with diagrams.Diagram('AWS Auditor', show=False, direction='TB'):
+with diagrams.Diagram('AWS Auditor', show=False, direction='LR'):
     auditor = diagrams.aws.compute.Lambda('AuditorLambdaFunction')
     (
         diagrams.aws.integration.Eventbridge('AuditSchedule') >>
         auditor >>
         diagrams.aws.database.DynamodbTable('AuditInventory')
     )
-    with diagrams.Cluster('IAM'):
-        auditor_role = diagrams.aws.security.IAMRole('AuditorLambdaRole')
-        (
-            diagrams.aws.security.IAMPermissions('ResourcePermissions') -
-            auditor_role >> diagrams.Edge() << auditor
-        )
+    # with diagrams.Cluster('IAM'):
+    #     auditor_role = diagrams.aws.security.IAMRole('AuditorLambdaRole')
+    #     (
+    #         diagrams.aws.security.IAMPermissions('ResourcePermissions') -
+    #         auditor_role >> diagrams.Edge() << auditor
+    #     )
