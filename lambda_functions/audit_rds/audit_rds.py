c42aeca9ab3e23ebb2d5977da02f5e6b4539b35a
jadecobra <jakeitegsy@yahoo.com>
2022-04-12 14:10:07 -0500

    update tests

--- lambda_functions/audit_rds/audit_rds.py
+++ lambda_functions/audit_rds/audit_rds.py
@@ -1,1 +1,11 @@
 import boto3
+
+
+RDS = boto3.session.Session(region_name=region()).client('rds')
+PAGINATED_LIST_OF_FUNCTIONS = LAMBDA.get_paginator('list_functions')
+TABLE = boto3.resource(
+    'dynamodb',
+    endpoint_url=f'https://dynamodb.{region()}.amazonaws.com'
+).Table(
+    os.environ.get('INVENTORY_TABLE_NAME')
+)
