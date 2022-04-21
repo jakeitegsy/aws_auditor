63d6c90a8e60b16ac1a8a988b90b175c31cb5d8f
jadecobra <jakeitegsy@yahoo.com>
2022-04-21 08:04:05 -0500

    remove unused functions

--- lambda_functions/audit_cloudwatch_alarms/audit_cloudwatch_alarms.py
+++ lambda_functions/audit_cloudwatch_alarms/audit_cloudwatch_alarms.py
@@ -2,57 +2,19 @@ import boto3
 import datetime
 import os
 
-
-class MetricAlarm:
-    def __init__(self, metric_alarm):
-        self.metric_alarm = metric_alarm
-
-    def get(self, key, value=None):
-        try:
-            return self.metric_alarm[key]
-        except KeyError:
-            return value
-
-    def alarm_name(self):
-        return self.get("AlarmName")
-
-    @staticmethod
-    def stringify(iterable):
-        return ", ".join(iterable)
-
-    # def to_dict(self):
-    #     return {
-    #         'ResourceName': self.alarm_name(),
-    #         'DateAudited': str(datetime.datetime.now()),
-    #         'AlarmArn': self.get('AlarmArn'),
-    #         'AlarmDescription': self.get('AlarmDescription'),
-    #         'AlarmConfigurationUpdatedTimestamp': str(self.get('AlarmConfigurationUpdatedTimestamp')),
-    #         'ActionsEnabled': self.get('ActionsEnabled'),
-    #         'AlarmActions': self.stringify(self.get('AlarmActions')),
-    #         'OKActions': self.get('OKActions'),
-    #         'InsufficientDataActions': self.get('InsufficientDataActions'),
-    #         'StateValue': self.get('StateValue'),
-    #         'StateReason': self.get('StateReason'),
-    #         'MetricName': self.get('MetricName'),
-    #         'Namespace': self.get('Namespace'),
-    #         'Statistic': self.get('Statistic'),
-    #         'ExtendedStatistic': self.get('ExtendedStatistic'),
-    #         'Period': self.get('Period'),
-    #         'EvaluationPeriods': self.get('EvaluationPeriods'),
-    #         'Unit': self.get('Unit'),
-    #         'DatapointsToAlarm': self.get('DatapointsToAlarm'),
-    #         'Threshold': str(self.get('Threshold')),
-    #         'ComparisonOperator': self.get('ComparisonOperator'),
-    #     }
-
-    def to_dict(self):
-        for key, value in self.metric_alarm.items():
-            if isinstance(value, datetime.datetime):
-                self.metric_alarm[key] = str(value)
+CLOUDWATCH = boto3.client("cloudwatch")
+PAGINATED_LIST_OF_ALARMS = CLOUDWATCH.get_paginator("describe_alarms")
+TABLE = boto3.resource(
+    "dynamodb",
+).Table(os.environ.get("INVENTORY_TABLE_NAME"))
 
 
-def region():
-    return os.environ.get("REGION")
+def to_dict(metric_alarm):
+    for key, value in metric_alarm.items():
+        if isinstance(value, datetime.datetime):
+            metric_alarm[key] = str(value)
+    metric_alarm.update({"DateAudited": str(datetime.datetime.now())})
+    return metric_alarm
 
 
 def list_alarms():
@@ -63,17 +25,6 @@ def list_alarms():
     )
 
 
-def write_to_dynamodb(data):
-    return TABLE.put_item(Item=MetricAlarm(data).to_dict())
-
-
 def handler(event, context):
-    for alarm in list_alarms():
-        TABLE.put_item(Item=MetricAlarm(alarm).to_dict())
-
-
-CLOUDWATCH = boto3.client("cloudwatch")
-PAGINATED_LIST_OF_ALARMS = CLOUDWATCH.get_paginator("describe_alarms")
-TABLE = boto3.resource(
-    "dynamodb", endpoint_url=f"https://dynamodb.{region()}.amazonaws.com"
-).Table(os.environ.get("INVENTORY_TABLE_NAME"))
+    for metric_alarm in list_alarms():
+        TABLE.put_item(Item=to_dict(metric_alarm))
