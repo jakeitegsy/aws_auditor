5a79d7c0734390d39a58f8f11c71305e886191dc
jadecobra <jakeitegsy@yahoo.com>
2022-04-21 07:57:37 -0500

    update dict

--- lambda_functions/audit_cloudwatch_alarms/audit_cloudwatch_alarms.py
+++ lambda_functions/audit_cloudwatch_alarms/audit_cloudwatch_alarms.py
@@ -4,7 +4,6 @@ import os
 
 
 class MetricAlarm:
-
     def __init__(self, metric_alarm):
         self.metric_alarm = metric_alarm
 
@@ -15,61 +14,66 @@ class MetricAlarm:
             return value
 
     def alarm_name(self):
-        return self.get('AlarmName')
+        return self.get("AlarmName")
 
     @staticmethod
     def stringify(iterable):
-        return ', '.join(iterable)
+        return ", ".join(iterable)
+
+    # def to_dict(self):
+    #     return {
+    #         'ResourceName': self.alarm_name(),
+    #         'DateAudited': str(datetime.datetime.now()),
+    #         'AlarmArn': self.get('AlarmArn'),
+    #         'AlarmDescription': self.get('AlarmDescription'),
+    #         'AlarmConfigurationUpdatedTimestamp': str(self.get('AlarmConfigurationUpdatedTimestamp')),
+    #         'ActionsEnabled': self.get('ActionsEnabled'),
+    #         'AlarmActions': self.stringify(self.get('AlarmActions')),
+    #         'OKActions': self.get('OKActions'),
+    #         'InsufficientDataActions': self.get('InsufficientDataActions'),
+    #         'StateValue': self.get('StateValue'),
+    #         'StateReason': self.get('StateReason'),
+    #         'MetricName': self.get('MetricName'),
+    #         'Namespace': self.get('Namespace'),
+    #         'Statistic': self.get('Statistic'),
+    #         'ExtendedStatistic': self.get('ExtendedStatistic'),
+    #         'Period': self.get('Period'),
+    #         'EvaluationPeriods': self.get('EvaluationPeriods'),
+    #         'Unit': self.get('Unit'),
+    #         'DatapointsToAlarm': self.get('DatapointsToAlarm'),
+    #         'Threshold': str(self.get('Threshold')),
+    #         'ComparisonOperator': self.get('ComparisonOperator'),
+    #     }
 
     def to_dict(self):
-        return {
-            'ResourceName': self.alarm_name(),
-            'DateAudited': str(datetime.datetime.now()),
-            'AlarmArn': self.get('AlarmArn'),
-            'AlarmDescription': self.get('AlarmDescription'),
-            'AlarmConfigurationUpdatedTimestamp': str(self.get('AlarmConfigurationUpdatedTimestamp')),
-            'ActionsEnabled': self.get('ActionsEnabled'),
-            'AlarmActions': self.stringify(self.get('AlarmActions')),
-            'OKActions': self.stringify(self.get('OKActions')),
-            'InsufficientDataActions': self.stringify(self.get('InsufficientDataActions')),
-            'StateValue': self.get('StateValue'),
-            'StateReason': self.get('StateReason'),
-            'MetricName': self.get('MetricName'),
-            'Namespace': self.get('Namespace'),
-            'Statistic': self.get('Statistic'),
-            'ExtendedStatistic': self.get('ExtendedStatistic'),
-            'Period': self.get('Period'),
-            'EvaluationPeriods': self.get('EvaluationPeriods'),
-            'Unit': self.get('Unit'),
-            'DatapointsToAlarm': self.get('DatapointsToAlarm'),
-            'Threshold': str(self.get('Threshold')),
-            'ComparisonOperator': self.get('ComparisonOperator'),
-        }
+        for key, value in self.metric_alarm.items():
+            if isinstance(value, datetime.datetime):
+                self.metric_alarm[key] = str(value)
 
 
 def region():
-    return os.environ.get('REGION')
+    return os.environ.get("REGION")
+
 
 def list_alarms():
     return (
-        alarm for page in PAGINATED_LIST_OF_ALARMS.paginate()
-        for alarm in page['MetricAlarms']
+        alarm
+        for page in PAGINATED_LIST_OF_ALARMS.paginate()
+        for alarm in page["MetricAlarms"]
     )
+
 
 def write_to_dynamodb(data):
     return TABLE.put_item(Item=MetricAlarm(data).to_dict())
 
+
 def handler(event, context):
     for alarm in list_alarms():
-        TABLE.put_item(
-            Item=MetricAlarm(alarm).to_dict()
-        )
+        TABLE.put_item(Item=MetricAlarm(alarm).to_dict())
+
 
 CLOUDWATCH = boto3.client("cloudwatch")
 PAGINATED_LIST_OF_ALARMS = CLOUDWATCH.get_paginator("describe_alarms")
 TABLE = boto3.resource(
-    'dynamodb',
-    endpoint_url=f'https://dynamodb.{region()}.amazonaws.com'
-).Table(
-    os.environ.get('INVENTORY_TABLE_NAME')
-)
+    "dynamodb", endpoint_url=f"https://dynamodb.{region()}.amazonaws.com"
+).Table(os.environ.get("INVENTORY_TABLE_NAME"))
--- tests/test_audit_cloudwatch_alarms.py
+++ tests/test_audit_cloudwatch_alarms.py
@@ -1,10 +1,11 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 import mock_boto3
 
-sys.modules['boto3'] = mock_boto3
-os.environ['INVENTORY_TABLE_NAME'] = 'audit-lambda'
+sys.modules["boto3"] = mock_boto3
+os.environ["INVENTORY_TABLE_NAME"] = "audit-lambda"
 
 import unittest
 import datetime
@@ -12,119 +13,105 @@ import lambda_functions.audit_cloudwatch_alarms.audit_cloudwatch_alarms
 
 
 class TestAuditRds(unittest.TestCase):
-
     def setUp(self):
         self.fixture = {
-            'AlarmName': 'AlarmName',
-            'AlarmArn': 'AlarmArn',
-            'AlarmDescription': 'AlarmDescription',
-            'AlarmConfigurationUpdatedTimestamp': datetime.datetime(2015, 1, 1),
-            'ActionsEnabled': True,
-            'OKActions': [
-                'OKAction1',
-                'OKActionN',
+            "AlarmName": "AlarmName",
+            "AlarmArn": "AlarmArn",
+            "AlarmDescription": "AlarmDescription",
+            "AlarmConfigurationUpdatedTimestamp": datetime.datetime(2015, 1, 1),
+            "ActionsEnabled": True,
+            "OKActions": [
+                "OKAction1",
+                "OKActionN",
             ],
-            'AlarmActions': [
-                'AlarmAction1',
-                'AlarmActionN',
+            "AlarmActions": [
+                "AlarmAction1",
+                "AlarmActionN",
             ],
-            'InsufficientDataActions': [
-                'InsufficientDataAction1',
-                'InsufficientDataActionN',
+            "InsufficientDataActions": [
+                "InsufficientDataAction1",
+                "InsufficientDataActionN",
             ],
-            'StateValue': 'OK',
-            'StateReason': 'string',
-            'StateReasonData': 'string',
-            'StateUpdatedTimestamp': datetime.datetime(2015, 1, 1),
-            'MetricName': 'string',
-            'Namespace': 'string',
-            'Statistic': 'Sum',
-            'ExtendedStatistic': 'string',
-            'Dimensions': [
+            "StateValue": "OK",
+            "StateReason": "string",
+            "StateReasonData": "string",
+            "StateUpdatedTimestamp": datetime.datetime(2015, 1, 1),
+            "MetricName": "string",
+            "Namespace": "string",
+            "Statistic": "Sum",
+            "ExtendedStatistic": "string",
+            "Dimensions": [
+                {"Name": "Dimension1", "Value": "Value1"},
+                {"Name": "Dimension2", "Value": "Value2"},
+            ],
+            "Period": 123,
+            "Unit": "Bits",
+            "EvaluationPeriods": 123,
+            "DatapointsToAlarm": 123,
+            "Threshold": 123.0,
+            "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+            "TreatMissingData": "string",
+            "EvaluateLowSampleCountPercentile": "string",
+            "Metrics": [
                 {
-                    'Name': 'Dimension1',
-                    'Value': 'Value1'
-                },
-                {
-                    'Name': 'Dimension2',
-                    'Value': 'Value2'
-                },
-            ],
-            'Period': 123,
-            'Unit': 'Bits',
-            'EvaluationPeriods': 123,
-            'DatapointsToAlarm': 123,
-            'Threshold': 123.0,
-            'ComparisonOperator': 'GreaterThanOrEqualToThreshold',
-            'TreatMissingData': 'string',
-            'EvaluateLowSampleCountPercentile': 'string',
-            'Metrics': [
-                {
-                    'Id': 'MetricId',
-                    'MetricStat': {
-                        'Metric': {
-                            'Namespace': 'Namespace',
-                            'MetricName': 'MetricName',
-                            'Dimensions': [
-                                {
-                                    'Name': 'Dimension1',
-                                    'Value': 'Value1'
-                                },
-                                {
-                                    'Name': 'DimensionN',
-                                    'Value': 'ValueN'
-                                },
-                            ]
+                    "Id": "MetricId",
+                    "MetricStat": {
+                        "Metric": {
+                            "Namespace": "Namespace",
+                            "MetricName": "MetricName",
+                            "Dimensions": [
+                                {"Name": "Dimension1", "Value": "Value1"},
+                                {"Name": "DimensionN", "Value": "ValueN"},
+                            ],
                         },
-                        'Period': 123,
-                        'Stat': 'string',
-                        'Unit': 'Seconds'
+                        "Period": 123,
+                        "Stat": "string",
+                        "Unit": "Seconds",
                     },
-                    'Expression': 'Expression',
-                    'Label': 'Label',
-                    'ReturnData': True,
-                    'Period': 123,
-                    'AccountId': 'AccountId'
+                    "Expression": "Expression",
+                    "Label": "Label",
+                    "ReturnData": True,
+                    "Period": 123,
+                    "AccountId": "AccountId",
                 },
             ],
-            'ThresholdMetricId': 'string'
+            "ThresholdMetricId": "string",
         }
         self.metric_alarm = lambda_functions.audit_cloudwatch_alarms.audit_cloudwatch_alarms.MetricAlarm(
             self.fixture
         )
 
     def test_database_name(self):
-        self.assertEqual(
-            self.metric_alarm.alarm_name(),
-            self.fixture['AlarmName']
-        )
+        self.assertEqual(self.metric_alarm.alarm_name(), self.fixture["AlarmName"])
 
     def test_dictionary(self):
         self.maxDiff = None
         actual = self.metric_alarm.to_dict()
-        actual.pop('DateAudited')
+        actual.pop("DateAudited")
         self.assertEqual(
             actual,
             {
-                'ActionsEnabled': True,
-                'AlarmActions': 'AlarmAction1, AlarmActionN',
-                'AlarmArn': 'AlarmArn',
-                'AlarmDescription': 'AlarmDescription',
-                'ComparisonOperator': 'GreaterThanOrEqualToThreshold',
-                'StateReason': 'string',
-                'StateValue': 'OK',
-                'DatapointsToAlarm': 123,
-                'EvaluationPeriods': 123,
-                'InsufficientDataActions': 'InsufficientDataAction1, InsufficientDataActionN',
-                'AlarmConfigurationUpdatedTimestamp': str(datetime.datetime(2015, 1, 1, 0, 0)),
-                'MetricName': 'string',
-                'Namespace': 'string',
-                'OKActions': 'OKAction1, OKActionN',
-                'ExtendedStatistic': 'string',
-                'Period': 123,
-                'ResourceName': 'AlarmName',
-                'Statistic': 'Sum',
-                'Threshold': str(123.0),
-                'Unit': 'Bits'
-            }
+                "ActionsEnabled": True,
+                "AlarmActions": "AlarmAction1, AlarmActionN",
+                "AlarmArn": "AlarmArn",
+                "AlarmDescription": "AlarmDescription",
+                "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+                "StateReason": "string",
+                "StateValue": "OK",
+                "DatapointsToAlarm": 123,
+                "EvaluationPeriods": 123,
+                "InsufficientDataActions": "InsufficientDataAction1, InsufficientDataActionN",
+                "AlarmConfigurationUpdatedTimestamp": str(
+                    datetime.datetime(2015, 1, 1, 0, 0)
+                ),
+                "MetricName": "string",
+                "Namespace": "string",
+                "OKActions": "OKAction1, OKActionN",
+                "ExtendedStatistic": "string",
+                "Period": 123,
+                "ResourceName": "AlarmName",
+                "Statistic": "Sum",
+                "Threshold": str(123.0),
+                "Unit": "Bits",
+            },
         )
